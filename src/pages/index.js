6b9264d25171f782ed60ff2243888897fe8ff0a1
Ed Brannin <edbrannin@gmail.com>
2019-08-25 20:19:50 -0400

    Move up the Dr. Ray invitation

--- src/pages/index.js
+++ src/pages/index.js
@@ -64,13 +64,14 @@ const IndexPage = () => (
       {link}
     </p>
 
+    <h2>A short message from Dr. Ray Guarendi</h2>
+    <iframe src="https://player.vimeo.com/video/355821627" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
     <p>
       May Jesus, ever present in the most Holy Eucharist, remain the source and
       summit of our faith and lead us to live out our daily lives as Christian witnesses. 
     </p>
 
-    <h2>A short message from Dr. Ray Guarendi</h2>
-    <iframe src="https://player.vimeo.com/video/355821627" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
 
 <div style={{
   clear: 'both',
