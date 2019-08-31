c2c1f0119e8d44fe9bd5c3f86ac528ac46061c30
Ed Brannin <edbrannin@gmail.com>
2019-08-31 19:32:08 -0400

    Add Parish Men's Groups page

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('admin');
   eleventyConfig.addPassthroughCopy('assets');
   eleventyConfig.addPassthroughCopy('images');
+  eleventyConfig.addPassthroughCopy('favicons');
 
   eleventyConfig.setTemplateFormats([
     'html',
@@ -22,4 +23,22 @@ module.exports = function(eleventyConfig) {
     }
     return `<a href="${to}" class="nav-item nav-link ${cssClass} ${page.url == to ? 'active' : ''}">${text}</a>`;
   });
+
+  eleventyConfig.addShortcode("ExternalLink", function(href, text) {
+    if (!href && !text) {
+      return '';
+    }
+    if (!href) {
+      return `<span>${text}</span>`;
+    }
+    const fixedHref = href.includes('://') ? href : `http://${href}`;
+    return (
+      `<a href="${fixedHref}">${text || 'Website'}</a>`
+    );
+  });
+
+  eleventyConfig.addFilter("debug", function(value) {
+    return `<pre>${JSON.stringify(value, null, 2)}</pre>`;
+  });
+
 };
